00e545a2dd71a26c4aabf4982393989b245095a4
Siramest <siramest@gmail.com>
2021-11-02 16:25:08 -0400

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,56 @@
+package com.siramest;
+
+public class Array {
+    private int[] items;
+    private int count;
+
+    public Array(int length){
+        items = new int[length];
+    }
+
+    public void insert(int item){
+        // If the array is full, resize it
+        if (items.length == count){
+            // Create a new array (twice the size)
+            int[] newItems = new int[count * 2];
+
+            // Copy all the existing items
+            for (int i = 0; i < count; i++)
+                newItems[i] = items[i];
+
+            // Set "items" to this new array
+            items = newItems;
+        }
+
+        // Add the new item at the end
+        items[count++] = item;
+
+
+    }
+
+    public void removeAt(int index){
+        // Validate the index
+        if (index < 0 || index >= count)
+            throw new IllegalArgumentException();
+
+        // Sfhift the items to the left to fill the hole
+        for (int i = index; i < count; i++)
+            items[i] = items[i + 1];
+
+        count--;
+    }
+
+    public int indexOf(int item){
+        // If we find it, return index, otherwise, return -1
+        for (int i = 0; i < count; i++)
+            if (items[i] == item)
+                return i;
+
+        return -1;
+    }
+
+    public void print(){
+        for  (int i = 0; i < count; i++)
+            System.out.println(items[i]);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.siramest;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        System.out.println(numbers.indexOf(10));
+        numbers.print();
+    }
+}
